a27f5f677321c0dd9187f0ce2930e1363d77fd34
edgardodth <edgardodth@gmail.com>
2020-09-29 21:40:55 -0500

    calculando el numero e con una sumatoria

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+func factorial(f int) float64 {
+	fac := 1
+	for i:= 1; i < f+1; i++ {
+		fac = fac * i
+	}
+	return float64(fac)
+}
+
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var euler float64
+	var limit int
+
+	fmt.Scan(&limit)
+
+	for i:= 0; i <= limit; i++ {
+		euler += 1/factorial(i)
+	}
+
+	fmt.Print(euler)
 }
